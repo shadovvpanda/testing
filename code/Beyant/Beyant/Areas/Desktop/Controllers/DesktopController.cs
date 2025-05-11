d19ca018e37f4a1f5590ee382aa267c95c5351aa
shadovvpanda <ksa.007univ@gmail.com>
2025-05-11 16:35:47 +0530

    adding relevent folders

---
+++ code/Beyant/Beyant/Areas/Desktop/Controllers/DesktopController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Beyant.Areas.Desktop.Controllers
+{
+    public class DesktopController : Controller
+    {
+        public IActionResult Home()
+        {
+            return View();
+        }
+    }
+}
